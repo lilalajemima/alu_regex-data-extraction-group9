7c38fb6b3a5143240c4cae0b5ff91cc1c4b2ac66
Seyiranky <s.adebayo@alustudent.com>
2024-09-13 22:19:35 +0200

    new test data samples

--- regex.py
+++ regex.py
@@ -9,3 +9,14 @@ regex_patterns = {
     'hashtag': r'#\w+',
     'currency': r'\$\d{1,3}(,\d{3})*(\.\d{2})?'
 }
+New test data sample for each features test_data = {
+    'email': ['jane_doe23@website.net', 'contact@business.co.jp', 'support@domain.org'],
+    'url': ['https://shop.example.io/products', 'http://docs.sample-site.com/manual'],
+    'phone_number': ['(555) 123-4567', '987-654-3210', '555.987.6543'],
+    'credit_card': ['4321 8765 2109 6543', '4321-8765-2109-6543'],
+    'time_24h': ['09:15', '23:59'],
+    'time_12h': ['10:45 AM', '11:15 PM', '6:00 pm'],
+    'html_tag': ['<h1>Hello World</h1>', '<span class="highlight">Text</span>', '<a href="link.html">Click here</a>'],
+    'hashtag': ['#CoolTech', '#PythonRocks', '#100DaysOfCode'],
+    'currency': ['$5.00', '$999.99', '$12,345.67']
+}
