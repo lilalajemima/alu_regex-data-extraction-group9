0151dcff33b128799f0ad75806320c6f45f8bd1c
lilalajemima <169045583+lilalajemima@users.noreply.github.com>
2024-09-16 11:20:54 +0200

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # alu_regex-data-extraction-group9
+This is the repository for our hackathon project
+regex.py is a python script that extracts data from the pages of string response  
+All eight challenges are done that is 
+Email addresses
+URLs
+Phone numbers
+Credit card numbers
+Time in 12-hour or 24-hour format
+HTML tags
+Hashtags
+Currency amounts
--- regex.py
+++ regex.py
@@ -2,8 +2,8 @@ import re
 
 regex_patterns = {
     'email': r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+',
-    'url':r'https?://(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[\w\-./?%&=]*)?'
-    'phone_number': r'\(?\d{3}\)?[.\s-]?\d{3}[.\s-]?\d{4}',
+    'url':r'https?://(?:www\.)?[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:/[\w\-./?%&=]*)?',
+    'phone_number':r'\(?\d{3}\)?[.\s-]?\d{3}[.\s-]?\d{4}',
     'credit_card': r'\d{4}[ -]?\d{4}[ -]?\d{4}[ -]?\d{4}',
     'time_24h': r'([01]\d|2[0-3]):[0-5]\d',
     'time_12h': r'(1[0-2]|0?[1-9]):[0-5]\d\s?(AM|PM|am|pm)',
